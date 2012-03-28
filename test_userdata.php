31e7435642ec8802ea36d64549e67438d6c1bae1
Pieter-Paul Luijten <ppluijten@gmail.com>
2012-03-28 16:28:50 +0200

    Removed unnecessary data.

--- test_userdata.php
+++ test_userdata.php
@@ -1,14 +1,5 @@
 <?php
 
-/*$_COOKIE = array (
-  'PHPSESSID' => 'a9745ca4d084ca5ecd149464c9c3b1d2',
-  'bb_sessionhash' => '282af89e466231645cc9fe23b635ebfe',
-  'bb_lastvisit' => '1332882280',
-  'bb_lastactivity' => '0',
-  'bb_userid' => '589',
-  'bb_password' => 'fa028bcd806cebf2a6b75d0bce4dd6ed',
-);*/
-
 require_once "header.php";
 
 echo "<pre>" . var_export(User::getUserDataArray(), 1) . "</pre>";
