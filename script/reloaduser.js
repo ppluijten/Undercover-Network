b02a599ad82d5a6cbd206341c73818595acdbaea
Pieter-Paul Luijten <ppluijten@gmail.com>
2012-03-29 20:09:02 +0200

    Completed the login functionality to properly handle the logging in into vBulletin.

--- script/reloaduser.js
+++ script/reloaduser.js
@@ -5,5 +5,16 @@ function notifyParent() {
 function frameLoaded() {
     var iframe = document.getElementById('loginframe');
     iframe.src = 'http://www.undercover-network.nl/forum/forum.php';
-    setTimeout('location.reload(true);', 500);
+    iframe.onload = function(){
+       //alert('Logged in succesfuly!');
+       location.reload(true);
+    };
+}
+
+function showLoading() {
+    var loginform = document.getElementById('loginform');
+    loginform.style.display = 'none';
+
+    var loading = document.getElementById('loading');
+    loading.style.display = '';
 }
--- test_curl.php
+++ test_curl.php
@@ -4,14 +4,32 @@
 
 $ch = curl_init("http://www.undercover-network.nl/forum/forum.php");
 
+$header = array();
+$header[0]  = "Accept: text/xml,application/xml,application/xhtml+xml,";
+$header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+$header[]   = "Cache-Control: max-age=0";
+$header[]   = "Connection: keep-alive";
+$header[]   = "Keep-Alive: 300";
+$header[]   = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+$header[]   = "Accept-Language: en-us,en;q=0.5";
+$header[]   = "Pragma: "; // browsers keep this blank.
+
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.8.1.7) Gecko/20070914 Firefox/2.0.0.7');
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+/*curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 curl_setopt($ch, CURLOPT_COOKIESESSION, true);
 curl_setopt($ch, CURLOPT_FAILONERROR, false);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
-curl_setopt($ch, CURLOPT_HEADER, true);
+//curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);*/
 
 $result = curl_exec($ch);
 curl_close($ch);
